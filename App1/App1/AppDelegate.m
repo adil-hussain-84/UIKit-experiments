3136a2748277784d6354ba219d134bc32b8e4c90
Adil Hussain <>
2023-06-09 10:46:42 +0100

    (App1) Removed an unnecessary space from the `AppDelegate.m` file.

--- App1/App1/AppDelegate.m
+++ App1/App1/AppDelegate.m
@@ -25,7 +25,6 @@
     return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
 }
 
-
 - (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
     // Called when the user discards a scene session.
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
